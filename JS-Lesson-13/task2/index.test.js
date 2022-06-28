d66299b1161f27286391ca3588db360d5dbf9e04
Dima Bondariev <86620950+dbondariev@users.noreply.github.com>
2022-06-28 20:03:23 +0300

    Update index.test.js

--- JS-Lesson-13/task2/index.test.js
+++ JS-Lesson-13/task2/index.test.js
@@ -1,4 +1,4 @@
-import getSum, { getOddNumbers, getSquaredArray } from './calculator.js';
+import getSum, { getOddNumbers, getSquaredArray } from './index.js';
 
 it('Should get only odd numbers', () => {
     expect(getOddNumbers([1, 2, 3, 4])).toEqual([1, 3]);
