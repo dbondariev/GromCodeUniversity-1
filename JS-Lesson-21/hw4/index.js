b48a49bb57129aff03f9178a89057f5d148e985d
dbondariev <dima.bondariev@gmail.com>
2022-07-06 22:35:50 +0300

    lesson 21 hw4

--- JS-Lesson-21/hw4/index.js
+++ JS-Lesson-21/hw4/index.js
@@ -1,6 +1,3 @@
-const cloneArr = (arr) => !Array.isArray(arr) ? null : arr.slice();
-
-const arr = [1, 5, 3, 4, 5];
-
-console.log(cloneArr(arr));
-console.log(arr);
+export const setTitle = title => {
+    document.querySelector('.title').textContent = text;
+}
