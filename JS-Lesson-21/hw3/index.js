f35a2c33dbe3c8eb1adca28bf09d7762c1f6eda5
dbondariev <dima.bondariev@gmail.com>
2022-07-06 22:29:27 +0300

    commmit yo

--- JS-Lesson-21/hw3/index.js
+++ JS-Lesson-21/hw3/index.js
@@ -1,17 +1,22 @@
-export const getTittle = () => {
+export const getTitle = () => {
     return document.querySelector('.title').textContent;
-}
+};
+
 export const getDescription = () => {
     return document.querySelector('.about').innerText;
-}
+};
+
 export const getPlans = () => {
-    return document.querySelectorAll('.plans').innerHTML;
-}
+    return document.querySelector('.plans').innerHTML;
+};
+
 export const getGoal = () => {
     return document.querySelector('.goal').outerHTML;
-}
+};
 
 console.log(getTittle());
 console.log(getDescription());
 console.log(getPlans());
 console.log(getGoal());
+
+
