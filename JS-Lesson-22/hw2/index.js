2ec59c234ea6576fded12b20d609b1aee4c248d2
dbondariev <dima.bondariev@gmail.com>
2022-07-07 14:52:12 +0300

    lesson 22 hw2

--- JS-Lesson-22/hw2/index.js
+++ JS-Lesson-22/hw2/index.js
@@ -1,14 +1,3 @@
-export const getItemsList = () => {
-    const elementsList = document.querySelectorAll('.technology');
-    console.dir(elementsList);
-    return elementsList;
-};
-getItemsList();
-
-export const getItemsArray = () => {
-    const elementsList = document.querySelectorAll('.tool');
-    const elementsArray = Array.from(elementsList);
-    console.dir(elementsArray);
-    return elementsArray;
-};
-getItemsList();
+document.querySelector('.text-input').addEventListener('change', function () {
+    console.log(this.value);
+});
