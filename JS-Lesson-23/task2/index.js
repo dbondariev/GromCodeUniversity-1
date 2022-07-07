ab17ee1e9737b5bb18c434d12aba8f5b12c620a6
dbondariev <dima.bondariev@gmail.com>
2022-07-07 15:47:12 +0300

    lesson 23 task2

--- JS-Lesson-23/task2/index.js
+++ JS-Lesson-23/task2/index.js
@@ -1,33 +1,70 @@
-export class Vehicle {
-    constructor(name, hasWheels) {
-        this.hasWheels = hasWheels;
-        this.name = name;
+const generateNumbersRange = (from, to) => {
+    const result = [];
+    for (let i = from; i <= to; i++) {
+        result.push(i);
+    }
+    return result;
+};
+
+const getLinesSeats = () =>
+    generateNumbersRange(1, 10)
+        .map(
+            siteNumber => `
+    <div
+    class="sector__seat"
+    data-seat-number="${siteNumber}"></div>`,
+        )
+        .join('');
+
+const getSectorLines = () => {
+    const seatsString = getLinesSeats();
+
+    return generateNumbersRange(1, 10)
+        .map(
+            lineNumber => `
+    <div
+    class="sector__line"
+    data-line-number="${lineNumber}"
+    >${seatsString}</div>
+  `,
+        )
+        .join('');
+};
+
+const arenaElem = document.querySelector('.arena');
+
+const renderArena = () => {
+    const linesString = getSectorLines();
+
+    const sectorsString = generateNumbersRange(1, 3)
+        .map(
+            sectorNumber => `
+    <div
+    class="sector"
+    data-sector-number="${sectorNumber}"
+    >${linesString}</div>
+  `,
+        )
+        .join('');
+
+    arenaElem.innerHTML = sectorsString;
+};
+
+const onSeatSelect = event => {
+    const isSeat = event.target.classList.contains('sector__seat');
+
+    if (!isSeat) {
+        return;
     }
 
-    move() {
-        console.log(`${this.name} is moving`);
-    }
+    const seatNumber = event.target.dataset.seatNumber;
+    const lineNumber = event.target.closest('.sector__line').dataset.lineNumber;
+    const sectorNumber = event.target.closest('.sector').dataset.sectorNumber;
 
-    stop() {
-        console.log(`${this.name} stopped`);
-    }
-}
+    const selectedSeatElem = document.querySelector('.board__selected-seat');
+    selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`;
+};
 
-class Ship extends Vehicle {
-    constructor(name, speed) {
-        super(name, false);
-        this.speed = speed;
-    }
+arenaElem.addEventListener('click', onSeatSelect);
 
-    move() {
-        console.log(`${this.name} lifting anchor up`);
-        super.move(); 
-    }
-
-    stop() {
-        console.log(`${this.name} lifting anchor down`);
-        super.stop();
-    }
-}
-
-console.log(new Ship('Titanic', 10).move());
+renderArena();
