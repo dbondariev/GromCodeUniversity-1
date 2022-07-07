b13898530342a91de2c298c2bf0a834a7be719a7
dbondariev <dima.bondariev@gmail.com>
2022-07-07 15:06:47 +0300

    lesson 22 hw8

--- JS-Lesson-22/hw8/index.js
+++ JS-Lesson-22/hw8/index.js
@@ -1,20 +1,9 @@
-export const finishList = () => {
-    const listItems = document.querySelector('.list');
-    const specialItem = listItems.querySelector('.special');
-    const firstItem = document.createElement('li');
-    const lastItem = document.createElement('li');
-    const beforeItem = document.createElement('li');
-    const afterItem = document.createElement('li');
 
-    firstItem.textContent = 1;
-    listItems.prepend(firstItem);
 
-    lastItem.textContent = 8;
-    listItems.append(lastItem);
-
-    beforeItem.textContent = 4;
-    specialItem.before(beforeItem);
-
-    afterItem.textContent = 6;
-    specialItem.after(afterItem);
-};
+document.querySelector('.task-status').addEventListener('change', function () {
+    if (this.checked) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+});
