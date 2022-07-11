109b857ad17d8f725ebf61d79c04b50bb67ff0c1
dbondariev <dima.bondariev@gmail.com>
2022-07-11 12:22:48 +0300

    Added Algorithm to lesson 13 , but only taks 1, since other tasks were tested with jest

--- JS-Lesson-13/task1/index.js
+++ JS-Lesson-13/task1/index.js
@@ -1,1 +1,7 @@
+// Algorithm:
+// 1. Create a fucntion that takes two argumets (arr, delta)
+// 2. Return a distructed array applying map method to only add delta to even numbers
+
+//Dont forget to test your code
+
 const filter = (arr, delta) => [...arr].map(el => (el % 2 === 0 ? el + delta : el));
