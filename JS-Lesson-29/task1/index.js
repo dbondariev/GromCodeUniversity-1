8846e36098252403bce1e5a6f5ef8de321a8cf4e
dbondariev <dima.bondariev@gmail.com>
2022-07-12 19:51:10 +0300

    Added Error

--- JS-Lesson-29/task1/index.js
+++ JS-Lesson-29/task1/index.js
@@ -9,17 +9,27 @@ const addImage = (imgSrc, callback) => {
 
     const onImageLoaded = () => {
         const { width, height } = imgElem;
-        callback ({ width, height });
+        callback (null,{ width, height });
     };
 
     imgElem.addEventListener('load', onImageLoaded);
+
+    imgElem.addEventListener('error', () => {
+        callback('Image load is failed');
+    }
+    );
 
 }
 
 const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
 
-const onImageLoaded = ({ width, height }) => {
+const onImageLoaded = (error, data) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    const { width, height } = data;
     const sizeElem = document.querySelector('.image-size');
     sizeElem.textContent = `${width} x ${height}`;
 };
