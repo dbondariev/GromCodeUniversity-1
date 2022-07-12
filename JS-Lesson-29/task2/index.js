0c82a79223e7d3bc964d8052d08d2674ebc111f3
dbondariev <dima.bondariev@gmail.com>
2022-07-12 20:21:43 +0300

    fixing bug

--- JS-Lesson-29/task2/index.js
+++ JS-Lesson-29/task2/index.js
@@ -29,15 +29,16 @@
 
 export const pinger = (count, period) => {
     let counter = count;
+    console.log('Ping');
     const interval = setInterval(() => {
-        if(counter-- > 0) {
-            console.log(`Ping`);
-        }
-        else {
+        counter -= 1;
+        if (counter > 0) {
+            console.log('Ping');
+        } else {
             clearInterval(interval);
         }
     }, period);
-}
+};
 // examples
 // pinger(5, 100); // makes 5 writes with 100 ms interval
 // pinger(7, 1500); // makes 7 writes with 1500 ms interval
