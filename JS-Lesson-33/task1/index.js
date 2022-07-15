9ca048cbe046538eed94577a4048313713f8643c
dbondariev <dima.bondariev@gmail.com>
2022-07-15 13:28:42 +0300

    fixed 33

--- JS-Lesson-33/task1/index.js
+++ JS-Lesson-33/task1/index.js
@@ -1,19 +1,9 @@
-
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 export function getTasksList() {
-  return fetch(baseUrl).then(arrayTasks => arrayTasks.json());  
+  return fetch(baseUrl).then(arrayTasks => arrayTasks.json());
 }
 
-function getTaskById(taskId) {
+export function getTaskById(taskId) {
   return fetch(`${baseUrl}/${taskId}`).then(objTask => objTask.json());
 }
-
-// examples
-getTasksList().then(tasksList => {
-  console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
-});
-
-getTaskById('2').then(taskData => {
-  console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
-});
