f8c3b56d14190ba867fdd5e7d96b9a2d8e9673fa
dbondariev <dima.bondariev@gmail.com>
2022-07-19 14:03:41 +0300

    commit lesson 35

--- JS-Lesson-24/extra1/index.js
+++ JS-Lesson-24/extra1/index.js
@@ -1,15 +1,22 @@
-export const studentsBirthDays = (students) => {
-    const result = [];
-    for (let i = 0; i < students.length; i++) {
-        result.push(`name: ${students[i].name}, birhDate: ${students[i].birthDate}`);
+const students = [
+    { name: 'Tom', birthDate: '01/15/2010' },
+    { name: 'Ben', birthDate: '01/17/2008' },
+    { name: 'Sam', birthDate: '03/15/2010' }]
+
+
+// console.log(studentsBirthDays(students));
+
+export const studentsBirthDays = (arr)  => {
+    return arr.map(item => {
+        const date = new Date(item.birthDate);
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${item.name} was born on ${month}/${day}/${year}`;
     }
-    return result;
+    );
+    
 }
 
-const students = [
-{ name: 'Tom', birthDate: '01/15/2010' },
-{ name: 'Ben', birthDate: '01/17/2008' },
-{ name: 'Sam', birthDate: '03/15/2010' }]
-
-
 console.log(studentsBirthDays(students));
+
